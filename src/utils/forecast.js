cb408311783b2a6c9e7797dbc990c31581345b77
Lazam <sheena.a.lazam@accenture.com>
2020-06-10 20:07:10 +0800

    Update weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,11 @@ const forecast = (latitude,longitude,callback) => {
         } else if (body.error){
             callback ('Unable to find location', undefined)
         } else {
-            callback(undefined,body.current.weather_descriptions[0] +
-                            '. It is currently '+ body.current.temperature
-                            + ' degF. It feels like ' + body.current.feelslike +' degF.')
+            callback(undefined,
+                            'At ' + body.location.localtime + ', it is '
+                            +body.current.weather_descriptions[0] + '. The temperature is '+ body.current.temperature
+                            + ' degF and it feels like ' + body.current.feelslike +' degF. ' +
+                            'The humidity is '  + body.current.humidity +'.')
         }
     })
 }
